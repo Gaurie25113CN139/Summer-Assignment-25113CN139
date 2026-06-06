1eeb1035313fed73598db03217c2c301534716c8
Gaurie25113CN139 <gaurievarshney08@gmail.com>
2026-06-06 19:03:36 +0530

    DAY5

---
+++ 20.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int getMaxPrimeFactor(int n)
+{
+    int i, max = -1;
+    while (n % 2 == 0)
+    {
+        max = 2;
+        n /= 2;
+    }
+    for (i = 3; i <= sqrt(n); i += 2)
+    {
+        while (n % i == 0)
+        {
+            max = i;
+            n = n / i;
+        }
+    }
+    if (n > 2)
+    {
+        max = n;
+    }
+    return max;
+}
+
+int main()
+{
+    int n;
+    printf("enter a no.:");
+    scanf("%d", &n);
+
+    printf("max prime factor: %d", getMaxPrimeFactor(n));
+
+    return 0;
+}
