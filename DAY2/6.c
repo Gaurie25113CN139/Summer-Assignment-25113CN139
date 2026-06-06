f45cf5db06e5fc7f6e283dfb9dae0ee79b111a8f
Gaurie25113CN139 <gaurievarshney08@gmail.com>
2026-06-06 22:32:45 +0530

    Update 6.c

--- DAY2/6.c
+++ DAY2/6.c
@@ -9,18 +9,8 @@ int main() {
     rem = n % 10;
     rev = rev * 10 + rem;
     n /= 10;
-  }
-  if (original % 10 == 0) {
-    printf("reversed number = %d", rev);
-    
-    while (original % 10 == 0) {
-      printf("0");
-      original /= 10;
-    }
   } 
-  else {
-    printf("reversed no. = %d", rev);
-  }
-
+  
+  printf("reversed no. = %d", rev);
   return 0;
 }
