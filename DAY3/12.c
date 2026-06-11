a9ab247e4de837f57dec44270b572f5ecea04b12
Gaurie25113CN139 <gaurievarshney08@gmail.com>
2026-06-11 15:20:38 +0530

    Update 12.c

--- DAY3/12.c
+++ DAY3/12.c
@@ -6,7 +6,10 @@ int main()
     printf("enter two numbers: ");
     scanf("%d %d", &n1, &n2);
 
-    max = (n1 > n2) ? n1 : n2;
+    if(n1 > n2)
+    max = n1;
+else
+    max = n2;
     while (1)
     {
         if (max % n1 == 0 && max % n2 == 0)
